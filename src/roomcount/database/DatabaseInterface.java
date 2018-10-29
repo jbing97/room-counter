de34ccb147d3d0ee0e38a9c7f33244bef572da7a
Chris Thierauf <chris@cthierauf.com>
2018-10-29 15:13:11 -0400

    Add database stubs

--- src/roomcount/database/DatabaseInterface.java
+++ src/roomcount/database/DatabaseInterface.java
@@ -1,5 +1,53 @@
 package roomcount.database;
 
-public class DatabaseInterface {
+import java.sql.Time;
 
+public class DatabaseInterface {
+	public String getRoomName(){
+		return "";
+	}
+
+	public int getRoomCapacity() {
+		return 0;
+	}
+
+	public Time getTimeSlot() {
+		return null;
+	}
+
+	public String getSessionName() {
+		return "";
+	}
+
+	public int getSessionNumber() {
+		return 0;
+	}
+
+	public String getSpeakerName() {
+		return "";
+	}
+
+	public boolean setRoomName() {
+		return false;
+	}
+
+	public boolean setRoomCapacity() {
+		return false;
+	}
+
+	public boolean setTimeSlot() {
+		return false;
+	}
+
+	public boolean setSessionName() {
+		return false;
+	}
+
+	public boolean setSessionNumber() {
+		return false;
+	}
+
+	public boolean setSpeakerName() {
+		return false;
+	}
 }
