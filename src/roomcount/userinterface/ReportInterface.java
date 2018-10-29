c983f1da126575976e3e8cdc0babe6c98cc59508
Chris Thierauf <chris@cthierauf.com>
2018-10-29 15:20:00 -0400

    Add UI stubs

--- src/roomcount/userinterface/ReportInterface.java
+++ src/roomcount/userinterface/ReportInterface.java
@@ -1,5 +1,19 @@
 package roomcount.userinterface;
 
 public class ReportInterface {
+	public boolean createRoomDropdown() {
+		return false;
+	}
 
+	public boolean createRoomCountInput() {
+		return false;
+	}
+
+	public boolean checkifTimeSlotNeeded() {
+		return false;
+	}
+
+	public boolean checkifTimeSlotInput() {
+		return false;
+	}
 }
--- src/roomcount/userinterface/UserInterface.java
+++ src/roomcount/userinterface/UserInterface.java
@@ -1,5 +1,31 @@
 package roomcount.userinterface;
 
 public class UserInterface {
+	public void initUI() {
 
+	}
+
+	public boolean pushToDatabase() {
+		return false;
+	}
+
+	public boolean createDropDownMenu() {
+		return false;
+	}
+
+	public boolean createNumericalInput() {
+		return false;
+	}
+
+	public boolean createButton() {
+		return false;
+	}
+
+	public boolean populateDropDown() {
+		return false;
+	}
+
+	public boolean presentStatus() {
+		return false;
+	}
 }
