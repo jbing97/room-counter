5fe602ec1378beab9d1eee5ab5a3545b18a53370
jbing97 <41120990+jbing97@users.noreply.github.com>
2018-11-06 14:48:52 -0500

    Update Database_Init_Interface.java

--- src/roomcount/database/Database_Init_Interface.java
+++ src/roomcount/database/Database_Init_Interface.java
@@ -2,9 +2,11 @@ package roomcount.database;
 
 public class Database_Init_Interface {
 	
-	
-    private MongoDatabase _db = getConnection().getDatabase(database_name);
-    private MongoCollection _col = db.getCollection(database_collection);
+	//Database Objects
+    private MongoDatabase _db;
+    private MongoCollection _sessions;
+    private MongoCollection _rooms;
+    private MongoCollection _timeSlots;
 	
 	//Session Fields
 	private String _sessionName;
@@ -24,64 +26,100 @@ public class Database_Init_Interface {
 	private String _endTime;
 	
 	public void initInterface() {
-		
-		
-		
-		
-		
-	}
-	
+		_db = getConnection().getDatabase(database_name);
+		_sessions = db.getCollection("Sessions");
+		_rooms = db.getCollection("Rooms");
+		_timeSlots = db.getCollection("TimeSlots");
+	}	
     private static MongoClient getConnection() {
         int port_num = 8080;
         String url = "localhost";
-        
-        MongoClient mongoClntObj = new MongoClient(url, port_num);
-        return mongoClntObj;
+        return new MongoClient(url, port_num);
     }
-	
-	public boolean pushSessionDocument() {
+    
+	//Needs Validation
+	public boolean pushSessionDocument(String sessionName, String sessionNumber, String speaker, String roomCount1, String roomCount2, String roomCount3) {
 		
+		//Validation Check
 		
+		List<BasicDBObject> sessionDoc = new ArrayList<BasicDBObject>();
+		sesh.add(new BasicDBObject("sessionName", _sessionName));
+		sesh.add(new BasicDBObject("sessionID", _sessionNumber));
+		sesh.add(new BasicDBObject("speakerName", _speaker));
+		sesh.add(new BasicDBObject("speakerName", _roomCount1));
+		sesh.add(new BasicDBObject("speakerName", _roomCount2));
+		sesh.add(new BasicDBObject("speakerName", _roomCount3));	
 		
-		
-		
-        List<BasicDBObject> sessionDoc = new ArrayList<BasicDBObject>();
-        sesh.add(new BasicDBObject("sessionName", _sessionName));
-        sesh.add(new BasicDBObject("sessionID", _sessionNumber));
-        sesh.add(new BasicDBObject("speakerName", _speaker));
-        sesh.add(new BasicDBObject("speakerName", _roomCount1));
-        sesh.add(new BasicDBObject("speakerName", _roomCount2));
-        sesh.add(new BasicDBObject("speakerName", _roomCount3));	
+	    if (isNewSession()){
+	    	
+	    	//Send to database
+	    	
+	    	return true;
+		}
+	    
 		return false;
 	}
 	
-	public boolean pushRoomDocument() {
+	//Needs Validation
+	public boolean pushRoomDocument(String roomName, String roomCapacity) {
 		
+		//Validation Check
 		
 		List<BasicDBObject> roomDoc = new ArrayList<BasicDBObject>();
 		roomDoc.add(new BasicDBObject("_roomName", roomName));
 		roomDoc.add(new BasicDBObject("_capacity", roomCapacity));
-		return false;
-	}
-	
-	public boolean pushTimeslotDocument() {
 		
+	    if (isNewRoom()){
+	    	
+	    	//Send to database
+	    	
+	    	return true;
+		}
+	    
+		return false;
+	}	
+	
+	//Needs Validation
+	public boolean pushTimeslotDocument(String startTime, String endTime, String date) {
+		
+		//Validation Check
 		
 		List<BasicDBObject> timeSlotDoc = new ArrayList<BasicDBObject>();
 		timeSlotDoc.add(new BasicDBObject("_date", startTime));
 		timeSlotDoc.add(new BasicDBObject("_startTime", endTime));
 		timeSlotDoc.add(new BasicDBObject("_endTime", date));
-        
+		
+	    if (isNewSession()){
+	    	
+	    	//Send to database
+	    	
+	    	return true;
+		}
+	    
 		return false;
 	}
 	
-	
+	//Needs to be completed
 	private boolean isNewSession() {
         FindIterable<Document> cursor = col.find(whereQuery);
         for(Document doc : cursor){
             //Session has already been created
-            newSession = false;
         }
 	}
-
+	
+	//Needs to be completed
+	private boolean isNewRoom() {
+        FindIterable<Document> cursor = col.find(whereQuery);
+        for(Document doc : cursor){
+            //Session has already been created
+        }
+	}
+	
+	//Needs to be completed
+	private boolean isNewTimeSlot() {
+        FindIterable<Document> cursor = col.find(whereQuery);
+        for(Document doc : cursor){
+            //Session has already been created
+        }
+	}
 }
